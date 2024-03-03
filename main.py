fb16ec8427f275f2388156bdb71553219a822e34
Babak <103176930+0xbabak@users.noreply.github.com>
2024-03-03 12:38:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import os
 def matrix_print(matrix, size):
     for i in range(size):
         for j in range(size):
-            print(matrix[i][j], end='  ')
+            print(matrix[i][j], end='    ')
+        print('')
         print('')
 
 #generate a random cordinate of x and y
@@ -24,6 +25,9 @@ def shift_left(matrix, size):
                     matrix[x][y-1] = matrix[x][y]
                     matrix[x][y] = 0
                     y = y - 1
+                if y >= 1 and y < size and matrix[x][y-1] == matrix[x][y]:
+                    matrix[x][y-1] = matrix[x][y-1] * 2
+                    matrix[x][y] = 0
     return matrix 
 
 
@@ -37,6 +41,9 @@ def shift_right(matrix, size):
                     matrix[x][y+1] = matrix[x][y]
                     matrix[x][y] = 0
                     y = y + 1
+                if y >= 0 and y < size - 1 and matrix[x][y+1] == matrix[x][y]:
+                    matrix[x][y+1] = matrix[x][y+1] * 2
+                    matrix[x][y] = 0
     return matrix 
 
 def shift_up(matrix, size):
@@ -49,6 +56,9 @@ def shift_up(matrix, size):
                     matrix[x-1][y] = matrix[x][y]
                     matrix[x][y] = 0
                     x = x - 1
+                if x >= 1 and x < size and matrix[x-1][y] == matrix[x][y]:
+                    matrix[x-1][y] = matrix[x-1][y] * 2
+                    matrix[x][y] = 0                   
     return matrix 
 
 def shift_down(matrix, size):
@@ -61,6 +71,9 @@ def shift_down(matrix, size):
                     matrix[x+1][y] = matrix[x][y]
                     matrix[x][y] = 0
                     x = x + 1
+                if x >= 0 and x < size - 1 and matrix[x+1][y] == matrix[x][y]:
+                    matrix[x+1][y] = matrix[x+1][y] * 2
+                    matrix[x][y] = 0            
     return matrix 
 
 def get_key():
@@ -74,15 +87,12 @@ def get_key():
 
 print("WELCOME TO 2048")
 
-# Set a default value for high_score if it's not already defined
-high_score = 0 if 'high_score' not in locals() else high_score
+high_score = 0
 
-print(f"High Score: {high_score}")
+key = input("Press any button to play | Press Q to quit: ")
 
-ch = input("Press any button to play | Press Q to quit: ")
-
-while ch.upper() != "Q":
-    # Get user input for area size
+while key.upper() != "Q":
+    print(f"High Score: {high_score}")
     area_size = int(input("Please enter an area size: "))
     matrix = np.zeros((area_size, area_size), dtype=int)
     x , y = coordinate_generate(area_size)
@@ -95,12 +105,13 @@ while ch.upper() != "Q":
     os.system("cls" if os.name == "nt" else "clear")
 
     matrix_print(matrix, area_size)
-    import keyboard
 
-    while True:
+    while key.upper() != "Q":
         key = get_key().decode('utf-8')
+        while key != 'w' and key != 'd' and key != 'a' and key != 's':
+            print("Please enter a valid key | W A S D")
+            key = get_key().decode('utf-8')
         os.system("cls" if os.name == "nt" else "clear")  # Clear the console screen
-
         if key == 'w':
             shift_up(matrix, area_size)
         elif key == 'd':
@@ -115,4 +126,7 @@ while ch.upper() != "Q":
             a, b = coordinate_generate(area_size)
         matrix[a][b] = 2
         matrix_print(matrix, area_size)
+        high_score = high_score + 1
+
+    key = input("Do you want to play again? press any button | Press Q to quit: ")
 
